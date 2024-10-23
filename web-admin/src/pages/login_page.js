125caa52ce56d4fec1e6cd84c55cbb50a73a5a1a
vuongq303 <vuongq303@gmail.com>
2024-10-23 17:56:14 +0700

    FE: add admin-page, user, product

--- web-admin/src/pages/login_page.js
+++ web-admin/src/pages/login_page.js
@@ -1,19 +1,51 @@
-import React from "react";
+import axios from "axios";
+import React, { useRef } from "react";
 import { useNavigate } from "react-router-dom";
+import url_config from "../config.json";
 
 export default function LoginPage() {
   const navigation = useNavigate();
+  const username = useRef();
+  const password = useRef();
+  const btnLogin = useRef();
 
-  function loginForm() {
-    navigation("/user");
+  async function loginForm() {
+    try {
+      const {
+        status,
+        data: { isAdmin, response, type },
+      } = await axios.post(url_config[0].url_connect + "/admin/login", {
+        username: username.current.value,
+        password: password.current.value,
+      });
+
+      if (status == 200) {
+        window.alert(response);
+        if (type) {
+          window.localStorage.setItem("@isAdmin", isAdmin);
+          navigation("/user", { replace: true });
+        }
+      }
+    } catch (error) {
+      console.log(error);
+    }
   }
 
   return (
     <div className="m-2 d-flex flex-column justify-content-center align-items-center vh-100">
       <h2>Login</h2>
-      <input className="col-md-2 mb-2" placeholder="Username" />
-      <input className="col-md-2 mb-2" type="password" placeholder="Password" />
-      <button onClick={loginForm} className="col-md-1">
+      <input ref={username} className="col-md-2 mb-2" placeholder="Username" />
+      <input
+        ref={password}
+        className="col-md-2 mb-2"
+        type="password"
+        placeholder="Password"
+      />
+      <button
+        ref={btnLogin}
+        onClick={loginForm}
+        className="col-md-1 btn btn-success"
+      >
         Login
       </button>
     </div>
