a7dfce6eb1f55dc3c21cc5b170166fc1ac1991d3
vuongq303 <vuongq303@gmail.com>
2024-10-26 20:39:44 +0700

    FE: edit cart screen,

--- Layout/CartScreen.js
+++ Layout/CartScreen.js
@@ -1,13 +1,23 @@
-import React, { useEffect, useState } from 'react';
-import { ScrollView, StyleSheet, Text, TouchableOpacity, View, ToastAndroid, Image, Modal, Pressable } from 'react-native';
-import axios from 'axios';
-import { URL } from './HomeScreen'; // Đảm bảo URL đã được định nghĩa
-import { numberUtils } from './utils/stringUtils';
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import React, { useEffect, useState } from "react";
+import {
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+  ToastAndroid,
+  Image,
+  Modal,
+  Pressable,
+} from "react-native";
+import axios from "axios";
+import { URL } from "./HomeScreen"; // Đảm bảo URL đã được định nghĩa
+import { numberUtils } from "./utils/stringUtils";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const CartScreen = ({ navigation }) => {
   const [cartItems, setCartItems] = useState([]);
-  const [emailUser, setEmailUser] = useState('');
+  const [emailUser, setEmailUser] = useState("");
   const [modalVisible, setModalVisible] = useState(false);
   const [modalAllVisible, setModalAllVisible] = useState(false);
   const [itemToRemove, setItemToRemove] = useState(null);
@@ -15,7 +25,7 @@ const CartScreen = ({ navigation }) => {
 
   useEffect(() => {
     const fetchUserEmail = async () => {
-      const userEmail = await AsyncStorage.getItem('@UserLogin');
+      const userEmail = await AsyncStorage.getItem("@UserLogin");
       setEmailUser(userEmail);
       fetchCart(userEmail);
     };
@@ -32,25 +42,31 @@ const CartScreen = ({ navigation }) => {
       calculateTotalPrice(response.data);
     } catch (error) {
       console.error(error);
-      ToastAndroid.show('Không thể tải giỏ hàng!', ToastAndroid.SHORT);
+      ToastAndroid.show("Không thể tải giỏ hàng!", ToastAndroid.SHORT);
     }
   };
 
   const calculateTotalPrice = (items) => {
-    const total = items.reduce((sum, item) => sum + item.price * item.quantity, 0);
+    const total = items.reduce(
+      (sum, item) => sum + item.price * item.quantity,
+      0
+    );
     setTotalPrice(total);
   };
 
   const handleRemoveFromCart = async (itemId) => {
     try {
       await axios.delete(`${URL}/carts/removeFromCart/${itemId}`);
-      const updatedCart = cartItems.filter(item => item._id !== itemId);
+      const updatedCart = cartItems.filter((item) => item._id !== itemId);
       setCartItems(updatedCart);
       calculateTotalPrice(updatedCart);
-      ToastAndroid.show('Sản phẩm đã được xóa khỏi giỏ hàng!', ToastAndroid.SHORT);
+      ToastAndroid.show(
+        "Sản phẩm đã được xóa khỏi giỏ hàng!",
+        ToastAndroid.SHORT
+      );
     } catch (error) {
       console.error(error);
-      ToastAndroid.show('Không thể xóa sản phẩm!', ToastAndroid.SHORT);
+      ToastAndroid.show("Không thể xóa sản phẩm!", ToastAndroid.SHORT);
     }
   };
 
@@ -59,10 +75,13 @@ const CartScreen = ({ navigation }) => {
       await axios.delete(`${URL}/carts/removeAllFromCart/${emailUser}`);
       setCartItems([]);
       setTotalPrice(0);
-      ToastAndroid.show('Đã xóa tất cả sản phẩm khỏi giỏ hàng!', ToastAndroid.SHORT);
+      ToastAndroid.show(
+        "Đã xóa tất cả sản phẩm khỏi giỏ hàng!",
+        ToastAndroid.SHORT
+      );
     } catch (error) {
       console.error(error);
-      ToastAndroid.show('Không thể xóa tất cả sản phẩm!', ToastAndroid.SHORT);
+      ToastAndroid.show("Không thể xóa tất cả sản phẩm!", ToastAndroid.SHORT);
     }
     setModalAllVisible(false);
   };
@@ -71,7 +90,7 @@ const CartScreen = ({ navigation }) => {
   const increaseQuantity = async (itemId) => {
     try {
       await axios.patch(`${URL}/carts/increaseQuantity/${itemId}`);
-      const updatedCart = cartItems.map(item => {
+      const updatedCart = cartItems.map((item) => {
         if (item._id === itemId) {
           return { ...item, quantity: item.quantity + 1 };
         }
@@ -79,19 +98,18 @@ const CartScreen = ({ navigation }) => {
       });
       setCartItems(updatedCart);
       calculateTotalPrice(updatedCart);
-     
     } catch (error) {
       console.error(error);
-      ToastAndroid.show('Không thể tăng số lượng!', ToastAndroid.SHORT);
+      ToastAndroid.show("Không thể tăng số lượng!", ToastAndroid.SHORT);
     }
   };
 
   // Hàm giảm số lượng
   const decreaseQuantity = async (itemId) => {
     try {
-      if (cartItems.find(item => item._id === itemId).quantity > 1) {
+      if (cartItems.find((item) => item._id === itemId).quantity > 1) {
         await axios.patch(`${URL}/carts/decreaseQuantity/${itemId}`);
-        const updatedCart = cartItems.map(item => {
+        const updatedCart = cartItems.map((item) => {
           if (item._id === itemId) {
             return { ...item, quantity: item.quantity - 1 };
           }
@@ -99,13 +117,15 @@ const CartScreen = ({ navigation }) => {
         });
         setCartItems(updatedCart);
         calculateTotalPrice(updatedCart);
-
       } else {
-        ToastAndroid.show('Số lượng không thể giảm xuống 0!', ToastAndroid.SHORT);
+        ToastAndroid.show(
+          "Số lượng không thể giảm xuống 0!",
+          ToastAndroid.SHORT
+        );
       }
     } catch (error) {
       console.error(error);
-      ToastAndroid.show('Không thể giảm số lượng!', ToastAndroid.SHORT);
+      ToastAndroid.show("Không thể giảm số lượng!", ToastAndroid.SHORT);
     }
   };
 
@@ -113,68 +133,132 @@ const CartScreen = ({ navigation }) => {
     <View style={styles.container}>
       <View style={styles.header}>
         <TouchableOpacity onPress={() => navigation.goBack()}>
-          <Image style={{ width: 20, height: 20 }} source={require('../Image/back.png')} />
+          <Image
+            style={{ width: 20, height: 20 }}
+            source={require("../Image/back.png")}
+          />
         </TouchableOpacity>
-        <Text style={{ textAlign: 'center', fontSize: 18, fontWeight: 'bold' }}>Giỏ hàng</Text>
+        <Text style={{ textAlign: "center", fontSize: 18, fontWeight: "bold" }}>
+          Giỏ hàng
+        </Text>
         <TouchableOpacity onPress={() => setModalAllVisible(true)}>
-          <Image style={{ width: 26, height: 26 }} source={require('../Image/delete.png')} />
+          <Image
+            style={{ width: 26, height: 26 }}
+            source={require("../Image/delete.png")}
+          />
         </TouchableOpacity>
       </View>
       <Modal
         animationType="slide"
         transparent={true}
         visible={modalAllVisible}
-        onRequestClose={() => setModalAllVisible(false)}>
+        onRequestClose={() => setModalAllVisible(false)}
+      >
         <View style={styles.cardCotainer}>
           <View />
           <View style={styles.cardModal}>
             <Text style={styles.textBold}>Xác nhận xóa tất cả đơn hàng?</Text>
-            <Text style={{ fontSize: 14, color: 'gray', fontWeight: '400' }}>
+            <Text style={{ fontSize: 14, color: "gray", fontWeight: "400" }}>
               Thao tác này sẽ không thể khôi phục.
             </Text>
             <Pressable
               style={styles.btnModal}
-              onPress={handleRemoveAllFromCart}>
-              <Text style={{ color: 'white', fontWeight: 'bold', fontSize: 16 }}>Đồng ý</Text>
+              onPress={handleRemoveAllFromCart}
+            >
+              <Text
+                style={{ color: "white", fontWeight: "bold", fontSize: 16 }}
+              >
+                Đồng ý
+              </Text>
             </Pressable>
-            <Text onPress={() => setModalAllVisible(false)}
-              style={{ textDecorationLine: 'underline', fontWeight: 'bold', fontSize: 16 }}>Hủy bỏ</Text>
+            <Text
+              onPress={() => setModalAllVisible(false)}
+              style={{
+                textDecorationLine: "underline",
+                fontWeight: "bold",
+                fontSize: 16,
+              }}
+            >
+              Hủy bỏ
+            </Text>
           </View>
         </View>
       </Modal>
 
       <ScrollView style={{ flex: 1 }}>
         {cartItems.length > 0 ? (
-          cartItems.map(item => (
+          cartItems.map((item) => (
             <View key={item._id} style={styles.item}>
               <Image source={{ uri: item.img }} style={styles.image} />
-              <View style={{ padding: 10, justifyContent: 'space-between' }}>
-                <Text style={{ marginBottom: 1, fontWeight: 'bold' }}>{item.name}</Text>
-                <Text style={{ marginBottom: 1, fontWeight: 'bold', color: '#FF0000' }}>{numberUtils(item.price)}</Text>
-                <View style={{ flexDirection: 'row' }}>
-                  <TouchableOpacity onPress={() => decreaseQuantity(item._id)} style={styles.btn}>
-                    <Image source={require('../Image/subtract.png')} style={styles.icon} />
+              <View style={{ padding: 10, justifyContent: "space-between" }}>
+                <Text style={{ marginBottom: 1, fontWeight: "bold" }}>
+                  {item.name}
+                </Text>
+                <Text
+                  style={{
+                    marginBottom: 5,
+                    fontWeight: "bold",
+                    color: "#FF0000",
+                  }}
+                >
+                  {numberUtils(item.price)}
+                </Text>
+                <View style={{ flexDirection: "row", alignItems: "center" }}>
+                  <TouchableOpacity
+                    onPress={() => decreaseQuantity(item._id)}
+                    style={styles.btn}
+                  >
+                    <Image
+                      source={require("../Image/subtract.png")}
+                      style={styles.icon}
+                    />
                   </TouchableOpacity>
-                  <Text style={{ marginLeft: 10, marginRight: 10 }}>{item.quantity}</Text>
-                  <TouchableOpacity onPress={() => increaseQuantity(item._id)} style={styles.btn}>
-                    <Image source={require('../Image/add.png')} style={styles.icon} />
+                  <Text style={{ marginLeft: 10, marginRight: 10 }}>
+                    {item.quantity}
+                  </Text>
+                  <TouchableOpacity
+                    onPress={() => increaseQuantity(item._id)}
+                    style={styles.btn}
+                  >
+                    <Image
+                      source={require("../Image/add.png")}
+                      style={styles.icon}
+                    />
                   </TouchableOpacity>
-                  <TouchableOpacity onPress={() => {
-                    setItemToRemove(item);
-                    setModalVisible(true);
-                  }}>
-                    <Text style={{ textDecorationLine: 'underline', marginLeft: 20 }}>Xóa</Text>
+                  <TouchableOpacity
+                    onPress={() => {
+                      setItemToRemove(item);
+                      setModalVisible(true);
+                    }}
+                  >
+                    <Text
+                      style={{
+                        textDecorationLine: "underline",
+                        marginLeft: 20,
+                      }}
+                    >
+                      Xóa
+                    </Text>
                   </TouchableOpacity>
                   <Modal
                     animationType="slide"
                     transparent={true}
                     visible={modalVisible}
-                    onRequestClose={() => setModalVisible(false)}>
+                    onRequestClose={() => setModalVisible(false)}
+                  >
                     <View style={styles.cardCotainer}>
                       <View />
                       <View style={styles.cardModal}>
-                        <Text style={styles.textBold}>Xác nhận xóa đơn hàng?</Text>
-                        <Text style={{ fontSize: 14, color: 'gray', fontWeight: '400' }}>
+                        <Text style={styles.textBold}>
+                          Xác nhận xóa đơn hàng?
+                        </Text>
+                        <Text
+                          style={{
+                            fontSize: 14,
+                            color: "gray",
+                            fontWeight: "400",
+                          }}
+                        >
                           Thao tác này sẽ không thể khôi phục.
                         </Text>
                         <Pressable
@@ -182,11 +266,28 @@ const CartScreen = ({ navigation }) => {
                           onPress={() => {
                             handleRemoveFromCart(itemToRemove._id); // Xóa sản phẩm đã chọn
                             setModalVisible(false);
-                          }}>
-                          <Text style={{ color: 'white', fontWeight: 'bold', fontSize: 16 }}>Đồng ý</Text>
+                          }}
+                        >
+                          <Text
+                            style={{
+                              color: "white",
+                              fontWeight: "bold",
+                              fontSize: 16,
+                            }}
+                          >
+                            Đồng ý
+                          </Text>
                         </Pressable>
-                        <Text onPress={() => setModalVisible(false)}
-                          style={{ textDecorationLine: 'underline', fontWeight: 'bold', fontSize: 16 }}>Hủy bỏ</Text>
+                        <Text
+                          onPress={() => setModalVisible(false)}
+                          style={{
+                            textDecorationLine: "underline",
+                            fontWeight: "bold",
+                            fontSize: 16,
+                          }}
+                        >
+                          Hủy bỏ
+                        </Text>
                       </View>
                     </View>
                   </Modal>
@@ -198,9 +299,12 @@ const CartScreen = ({ navigation }) => {
           <View style={styles.emptyCartContainer}>
             <Text style={styles.emptyCartText}>
               Giỏ hàng rỗng
-              {'\n'}Thêm sản phẩm vào giỏ hàng
+              {"\n"}Thêm sản phẩm vào giỏ hàng
             </Text>
-            <TouchableOpacity onPress={() => navigation.navigate('SearchScreen')} style={styles.addButton}>
+            <TouchableOpacity
+              onPress={() => navigation.navigate("SearchScreen")}
+              style={styles.addButton}
+            >
               <Text style={styles.addButtonText}>Tìm kiếm sản phẩm</Text>
             </TouchableOpacity>
           </View>
@@ -208,13 +312,29 @@ const CartScreen = ({ navigation }) => {
       </ScrollView>
 
       {cartItems.length > 0 && (
-        <View style={{ width: '90%', marginVertical: 20, marginHorizontal: '5%', gap: 20 }}>
-          <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+        <View
+          style={{
+            width: "90%",
+            marginVertical: 20,
+            marginHorizontal: "5%",
+            gap: 20,
+          }}
+        >
+          <View
+            style={{ flexDirection: "row", justifyContent: "space-between" }}
+          >
             <Text>Tạm tính :</Text>
-            <Text style={{ fontSize: 17, fontWeight: 'bold' }}>{numberUtils(totalPrice)}</Text>
+            <Text style={{ fontSize: 17, fontWeight: "bold" }}>
+              {numberUtils(totalPrice)}
+            </Text>
           </View>
-          <TouchableOpacity onPress={() => { /* Thực hiện thanh toán */ }} style={styles.checkoutButton}>
-            <Text style={{ color: 'white' }}>Tiến hành thanh toán</Text>
+          <TouchableOpacity
+            onPress={() => {
+              /* Thực hiện thanh toán */
+            }}
+            style={styles.checkoutButton}
+          >
+            <Text style={{ color: "white" }}>Tiến hành thanh toán</Text>
           </TouchableOpacity>
         </View>
       )}
@@ -229,17 +349,17 @@ const styles = StyleSheet.create({
     gap: 16,
   },
   header: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
     paddingVertical: 20,
   },
   item: {
     height: 160,
-    flexDirection: 'row',
-    alignItems: 'center',
+    flexDirection: "row",
+    alignItems: "center",
     borderBottomWidth: 1,
-    width: '100%',
+    width: "100%",
     gap: 20,
   },
   image: {
@@ -260,18 +380,18 @@ const styles = StyleSheet.create({
     marginHorizontal: 2,
   },
   cardCotainer: {
-    height: '100%',
-    justifyContent: 'space-between',
-    alignItems: 'center',
+    height: "100%",
+    justifyContent: "space-between",
+    alignItems: "center",
   },
   cardModal: {
-    width: '90%',
+    width: "90%",
     marginBottom: 20,
-    backgroundColor: 'white',
+    backgroundColor: "white",
     borderRadius: 10,
     padding: 30,
-    alignItems: 'center',
-    shadowColor: '#000',
+    alignItems: "center",
+    shadowColor: "#000",
     shadowOffset: { width: 0, height: 2 },
     shadowOpacity: 0.25,
     shadowRadius: 4,
@@ -280,41 +400,41 @@ const styles = StyleSheet.create({
   btnModal: {
     padding: 14,
     borderRadius: 10,
-    backgroundColor: '#825640',
+    backgroundColor: "#825640",
     marginVertical: 20,
-    width: '100%',
-    alignItems: 'center',
+    width: "100%",
+    alignItems: "center",
   },
   textBold: {
     fontSize: 16,
-    fontWeight: '400',
+    fontWeight: "400",
   },
   checkoutButton: {
     borderRadius: 9,
     padding: 12,
-    alignItems: 'center',
-    backgroundColor: '#825640',
+    alignItems: "center",
+    backgroundColor: "#825640",
   },
   emptyCartContainer: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
     padding: 20,
   },
   emptyCartText: {
-    textAlign: 'center',
+    textAlign: "center",
     fontSize: 16,
-    color: 'black',
+    color: "black",
   },
   addButton: {
     marginTop: 10,
     padding: 10,
-    backgroundColor: '#825640',
+    backgroundColor: "#825640",
     borderRadius: 5,
   },
   addButtonText: {
-    color: 'white',
-    fontWeight: 'bold',
+    color: "white",
+    fontWeight: "bold",
   },
 });
 
--- Layout/HomeScreen.js
+++ Layout/HomeScreen.js
@@ -14,7 +14,7 @@ import React, { useEffect, useState } from "react";
 import SliderShow from "./components/SliderShow";
 import { numberUtils, upperCaseFirstItem } from './utils/stringUtils';
 
-export const URL = "http://192.168.1.3";
+export const URL = "http://192.168.138.239";
 
 const { width: screenWidth } = Dimensions.get("window");
 
